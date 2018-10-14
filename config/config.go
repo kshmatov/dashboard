5f041b9fc1cc2c452d08d092b0ba54c7921ff7d2
Kirill Shmatov <kshmatov@gmail.com>
2018-10-14 14:01:49 +0300

    Config initialisation

--- config/config.go
+++ config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/kshmatov/dashboard/types"
+)
+
+type Configuration struct {
+	Server types.Server
+	Postgres types.Database
+	LogFile string
+}
+
+func Init(fname string) (*Configuration, error) {
+	jconf, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return nil, err
+	}
+	c := Configuration{}
+	err = json.Unmarshal(jconf, &c)
+	if err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
